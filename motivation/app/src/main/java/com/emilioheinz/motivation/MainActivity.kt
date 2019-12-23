a903f6b1775ed6272f392c215b9536f3ad278305
emilioheinz <emiliosheinz@gmail.com>
2019-12-23 15:59:10 -0300

    Creating android project

---
+++ motivation/app/src/main/java/com/emilioheinz/motivation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.emilioheinz.motivation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
