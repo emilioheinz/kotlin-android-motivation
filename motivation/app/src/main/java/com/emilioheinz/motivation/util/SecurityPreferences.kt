6ef4fb9a9a408f1fbb9fad724164616bbf244af3
emilioheinz <emiliosheinz@gmail.com>
2019-12-23 20:55:26 -0300

    Creating shared preferences class

---
+++ motivation/app/src/main/java/com/emilioheinz/motivation/util/SecurityPreferences.kt
@@ -0,0 +1,19 @@
+package com.emilioheinz.motivation.util
+
+import android.content.Context
+import android.content.SharedPreferences
+
+class SecurityPreferences(context: Context) {
+
+    private val sharedPreferences: SharedPreferences =
+        context.getSharedPreferences("motivation_shared_pref", Context.MODE_PRIVATE)
+
+    fun storeString(key: String, value: String) {
+        sharedPreferences.edit().putString(key, value).apply()
+    }
+
+    fun getStoredString(key: String): String {
+        return sharedPreferences.getString(key, "").toString()
+    }
+
+}
